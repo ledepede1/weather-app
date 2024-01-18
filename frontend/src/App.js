a04ac590e499e777a09f7297d71ccb4052073a9a
Mikkel <mikkelhulstroem@gmail.com>
2024-01-18 21:10:11 +0100

    Restructured

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return ( 
+    <div className="App">
+      <header className="App-header">
+        <h1>Weather App</h1>
+      </header>
+
+      <header className="App-WeatherBox1">
+          <div id="weather-box"></div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
